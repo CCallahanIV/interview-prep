ae00165f40d81a2dcc9c641929685db2adc37a32
Ted Callahan <ted@textio.com>
2020-05-30 20:44:25 -0700

    Added space/time complexity documentation.

--- src/general/microsoft/copy_ll.py
+++ src/general/microsoft/copy_ll.py
@@ -6,9 +6,18 @@ class Node:
 
 
 def copy_linked_list(root: Node) -> Node:
+    """
+    This solution should run in O(N) time. We run through the original linked list twice, once to copy
+    data to a set of new nodes and create a mapping of old to new node.
+
+    Then a second time to shift the nxt and other pointers using the list.
+
+    This requires O(N) extra space as we store not only a copy of the new nodes, but a mapping of old to new.
+    """
     curr_original_node = root
     original_to_copy_node_map = {}
     while curr_original_node:
+        # O(N) where N is number of nodes
         new_node = Node(
             data=curr_original_node.data,
             nxt=curr_original_node.nxt,
@@ -19,6 +28,7 @@ def copy_linked_list(root: Node) -> Node:
 
     curr_original_node = root
     while curr_original_node:
+        # O(N) where N is number of nodes
         new_node = original_to_copy_node_map[curr_original_node]
         if new_node.nxt:
             new_node.nxt = original_to_copy_node_map[curr_original_node.nxt]
